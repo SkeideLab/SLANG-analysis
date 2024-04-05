6284774a2229eeccf7dd021b2c5fa29087ef4429
Alexander <alexander.enge@hu-berlin.de>
2024-04-05 17:05:23 +0200

    Add `fd_num` to pybids session table

--- scripts/pybids_sessions_table.py
+++ scripts/pybids_sessions_table.py
@@ -1,18 +1,41 @@
 from pathlib import Path
 
+import pandas as pd
 from bids import BIDSLayout
 
 derivatives_dir = Path(__file__).parent.parent.parent.resolve()
 output_dir = derivatives_dir / 'pybids'
 task = 'language'
+fd_threshs = [0.2, 0.5, 1.0, 2.4]
+
+
+def get_fd_num(confounds_file, fd_thresh):
+
+    confounds = pd.read_csv(confounds_file, sep='\t')
+    fd = confounds['framewise_displacement']
+    fd_num = fd[fd > fd_thresh].count()
+
+    return fd_num
+
 
 bids_dir = derivatives_dir.parent
-layout = BIDSLayout(bids_dir)
-
+fmriprep_dir = derivatives_dir / 'fmriprep'
+layout = BIDSLayout(bids_dir, derivatives=fmriprep_dir)
 
 sessions = layout.get_collections(task=task, level='session', types='scans',
                                   merge=True)
 sessions_df = sessions.to_df()
+
+confounds_files = sorted(list(layout.get('filename', task=task,
+                                         desc='confounds', suffix='timeseries',
+                                         extension='tsv')))
+assert len(confounds_files) == len(sessions_df)
+
+for fd_thresh in fd_threshs:
+    fd_nums = [get_fd_num(confounds_file, fd_thresh)
+               for confounds_file in confounds_files]
+    fd_thresh_underscore = str(fd_thresh).replace('.', '_')
+    sessions_df[f'fd_num_greater_{fd_thresh_underscore}'] = fd_nums
 
 output_filename = f'task-{task}_sessions.tsv'
 output_file = output_dir / output_filename
