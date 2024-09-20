b41b9e49359ca7acffe5105e9e5ca179b5ebfe2c
Alexander <alexander.enge@hu-berlin.de>
2024-09-20 13:32:37 +0200

    Add similarity plotting code and plots

--- univariate_plot.py
+++ univariate_plot.py
@@ -1,4 +1,3 @@
-from pathlib import Path
 from string import ascii_uppercase
 
 import matplotlib as mpl
@@ -6,10 +5,175 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
-from bids import BIDSLayout
 from nilearn.image import load_img, math_img
 from nilearn.plotting import plot_glass_brain
-from univariate import CONTRASTS, DERIVATIVES_DIR, SPACE, TASK, UNIVARIATE_DIR
+from similarity import ATLAS_FILE, GLASSER_ROIS, SIMILARITY_DIR, get_roi_imgs
+from univariate import CONTRASTS, SPACE, TASK, UNIVARIATE_DIR
+
+mpl.rcParams.update({"font.family": ["Open Sans"], "font.size": 12})
+
+STATS = {'z0': 'intercept', 'z1': 'linear change', 'z2': 'quadratic change'}
+
+AUD_CONTRASTS = sorted([c for c in CONTRASTS if c.startswith('audios')])
+VIS_CONTRASTS = sorted([c for c in CONTRASTS if c.startswith('images')])
+
+
+def main():
+    """Main function to plot all univariate and multivariate results."""
+
+    meta_file = UNIVARIATE_DIR / f'task-{TASK}_space-{SPACE}_desc-metadata.tsv'
+    meta_df = pd.read_csv(meta_file, sep='\t',
+                          dtype={'subject': str, 'session': str})
+
+    example_img = plot_univariate(meta_df)
+    plot_similarity(example_img)
+
+
+def plot_univariate(meta_df):
+    """Plots univariate results (significant auditory and visual clusters for
+    intercept, linear, and quadratic change) as glass brain plots."""
+
+    fig_dir = UNIVARIATE_DIR / 'figures'
+    fig_dir.mkdir(exist_ok=True)
+
+    for aud_contrast, vis_contrast in zip(AUD_CONTRASTS, VIS_CONTRASTS):
+
+        fig, axs = plt.subplots(nrows=3, ncols=2, figsize=(20.0, 9.0))
+
+        letter_ix = 0
+        for row_ix, (stat, stat_label) in enumerate(STATS.items()):
+
+            for col_ix, contrast in enumerate([aud_contrast, vis_contrast]):
+
+                suffix = f'{stat}-clusters'
+                clusters_filename = f'task-{TASK}_space-{SPACE}_desc-{contrast}_{suffix}.nii.gz'
+                clusters_file = UNIVARIATE_DIR / clusters_filename
+                clusters_img = load_img(clusters_file)
+
+                plt.subplots_adjust(wspace=0.05)
+
+                plot = plot_glass_brain(clusters_img, display_mode='lyrz',
+                                        colorbar=False, figure=fig,
+                                        axes=axs[row_ix, col_ix],
+                                        black_bg=False, cmap='blue_orange',
+                                        vmin=-10.0, vmax=10.0, plot_abs=False)
+
+                contrast_label = contrast.\
+                    replace('-', ' ').\
+                    replace('minus', 'vs.').\
+                    replace('audios', 'Spoken').\
+                    replace('images', 'Written').\
+                    replace('noise', 'low-level').\
+                    replace('pseudo', 'pseudowords')
+                letter = ascii_uppercase[letter_ix]
+                letter_ix += 1
+                title = f'$\\bf{{{letter}}}$  {contrast_label}, {stat_label}'
+                plot.title(title, color='black', bgcolor='white',
+                           x=0.002, y=1.1)
+
+        desc = aud_contrast.replace('audios-', '')
+        fig_filename = f'task-{TASK}_space-{SPACE}_desc-{desc}_clusters.png'
+        fig_file = fig_dir / fig_filename
+        fig.savefig(fig_file, dpi=600, bbox_inches='tight')
+
+        letter_ix = 6
+        fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(19.163, 4.0))
+        for col_ix, contrast in enumerate([aud_contrast, vis_contrast]):
+
+            plt.sca(axs[col_ix])
+
+            cluster_mask = math_img('img - img', img=clusters_img)
+            max_cluster_size = 0
+            max_cluster_stat = None
+
+            for stat in STATS:
+
+                if stat == 'z0':
+                    continue
+
+                suffix = f'{stat}-clusters'
+                clusters_filename = f'task-{TASK}_space-{SPACE}_desc-{contrast}_{suffix}.nii.gz'
+                clusters_file = UNIVARIATE_DIR / clusters_filename
+                clusters_img = load_img(clusters_file)
+                clusters_data = clusters_img.get_fdata()
+                clusters_data = clusters_data[clusters_data != 0.0]
+
+                if not clusters_data.any():
+                    continue
+
+                cluster_ixs, cluster_counts = np.unique(clusters_data,
+                                                        return_counts=True)
+                if cluster_counts.max() > max_cluster_size:
+                    max_cluster_ix = cluster_ixs[np.argmax(cluster_counts)]
+                    cluster_mask = math_img(f'img == {max_cluster_ix}',
+                                            img=clusters_img)
+                    max_cluster_stat = stat
+
+            if max_cluster_stat is None:
+
+                plt.axis('off')
+
+            else:
+
+                fig_filename = f'task-{TASK}_space-{SPACE}_desc-{contrast}_cluster-mask.png'
+                fig_file = fig_dir / fig_filename
+                plot_glass_brain(cluster_mask, output_file=fig_file,
+                                 display_mode='lyrz', colorbar=False, black_bg=False,
+                                 title=f'{contrast} cluster mask (from {max_cluster_stat})')
+
+                voxel_ixs = np.where(cluster_mask.get_fdata())
+
+                beta_dfs = []
+                for row in meta_df.itertuples():
+
+                    subject = row.subject
+                    session = row.session
+                    func_dir = UNIVARIATE_DIR / \
+                        f'sub-{subject}/ses-{session}/func'
+                    beta_filename = f'sub-{subject}_ses-{session}_task-{TASK}_space-{SPACE}_desc-{contrast}_effect_size.nii.gz'
+                    beta_file = func_dir / beta_filename
+                    beta_img = load_img(beta_file)
+                    betas = beta_img.get_fdata()[voxel_ixs]
+                    beta_df = pd.DataFrame({'subject': subject,
+                                            'session': session,
+                                            'time': row.time,
+                                            'time2': row.time2,
+                                            'beta': betas.mean()},
+                                           index=[0])
+                    beta_dfs.append(beta_df)
+
+                beta_df = pd.concat(beta_dfs, ignore_index=True)
+
+                sns.lineplot(beta_df, x='time', y='beta', hue='subject',
+                             palette='hls', marker='o', markeredgewidth=0,
+                             legend=False)
+
+                plt.subplots_adjust(wspace=0.15)
+                plt.xlim(-1.0, 17.0)
+                plt.xlabel('Time (months)')
+                plt.ylabel('BOLD amplitude (a.u.)')
+
+                bs = []
+                for stat in STATS:
+                    b_stat = stat.replace('z', 'b')
+                    b_filename = f'task-{TASK}_space-{SPACE}_desc-{contrast}_{b_stat}.nii.gz'
+                    b_file = UNIVARIATE_DIR / b_filename
+                    b_img = load_img(b_file)
+                    b_vals = b_img.get_fdata()[voxel_ixs]
+                    bs.append(b_vals.mean())
+                add_quadratic_line(*bs)
+
+                letter = ascii_uppercase[letter_ix]
+                letter_ix += 1
+                title = f'$\\bf{{{letter}}}$'
+                plt.title(title, color='black', x=-0.08, y=0.93)
+
+            desc = aud_contrast.replace('audios-', '')
+            fig_filename = f'task-{TASK}_space-{SPACE}_desc-{desc}_lineplot.png'
+            fig_file = fig_dir / fig_filename
+            fig.savefig(fig_file, dpi=600, bbox_inches='tight')
+
+    return clusters_img
 
 
 def add_quadratic_line(intercept, linear, quadratic):
@@ -21,158 +185,112 @@ def add_quadratic_line(intercept, linear, quadratic):
     plt.plot(x_vals, y_vals, '--', color='black')
 
 
-TEMPLATEFLOW_DIR = DERIVATIVES_DIR / 'templateflow'
+def plot_similarity(example_img):
 
-mpl.rcParams.update({"font.family": ["Open Sans"], "font.size": 12})
+    fig_dir = SIMILARITY_DIR / 'figures'
+    fig_dir.mkdir(exist_ok=True)
 
-template_dir = TEMPLATEFLOW_DIR / f'tpl-{SPACE}'
-template_filename = f'tpl-{SPACE}_res-01_T1w.nii.gz'
-anat_file = template_dir / template_filename
+    corr_df_filename = f'task-{TASK}_space-{SPACE}_desc-correlations.tsv'
+    corr_df_file = SIMILARITY_DIR / corr_df_filename
+    corr_df = pd.read_csv(corr_df_file, sep='\t')
 
-meta_file = UNIVARIATE_DIR / f'task-{TASK}_space-{SPACE}_desc-metadata.tsv'
-meta_df = pd.read_csv(meta_file, sep='\t',
-                      dtype={'subject': str, 'session': str})
+    stat_df_filename = f'task-{TASK}_space-{SPACE}_desc-stats.tsv'
+    stat_df_file = SIMILARITY_DIR / stat_df_filename
+    stat_df = pd.read_csv(stat_df_file, sep='\t')
 
-stats = {'z0': 'intercept', 'z1': 'linear change', 'z2': 'quadratic change'}
+    roi_imgs = get_roi_imgs(ATLAS_FILE, ref_img=example_img)
 
-fig_dir = UNIVARIATE_DIR / 'figures'
-fig_dir.mkdir(exist_ok=True)
+    all_roi_labels = list(corr_df['roi_label'].unique())
+    anat_roi_labels = list(GLASSER_ROIS.keys())
+    func_roi_labels = sorted([roi_label for roi_label in all_roi_labels
+                              if roi_label not in anat_roi_labels])
 
-auditory_contrasts = sorted([c for c in CONTRASTS if c.startswith('audios')])
-visual_contrasts = sorted([c for c in CONTRASTS if c.startswith('images')])
+    for pair_label, pair_df in corr_df.groupby('pair_label'):
 
-for auditory_contrast, visual_contrast in zip(auditory_contrasts, visual_contrasts):
+        condition = pair_label.split('-')[0]
+        this_func_roi_labels = [roi_label for roi_label in func_roi_labels
+                                if condition in roi_label]
+        this_roi_labels = anat_roi_labels + this_func_roi_labels
+        pair_df = pair_df[pair_df['roi_label'].isin(this_roi_labels)]
 
-    fig, axs = plt.subplots(nrows=3, ncols=2, figsize=(20.0, 9.0))
+        pair_df['roi_label'] = pair_df['roi_label'].\
+            astype('category').\
+            cat.set_categories(this_roi_labels)
 
-    letter_ix = 0
-    for row_ix, (stat, stat_label) in enumerate(stats.items()):
+        n_rois = len(this_roi_labels)
+        n_cols = 2
+        n_rows = n_rois // 2
+        figsize = (n_cols * 8.0, n_rois * 2.0)
+        fig, axs = plt.subplots(nrows=n_rows, ncols=n_cols, figsize=figsize)
 
-        for col_ix, contrast in enumerate([auditory_contrast, visual_contrast]):
+        axs = np.ravel(axs)
+        letter_ix = 0
+        for ix, (ax, (roi_label, roi_df)) in enumerate(zip(axs, pair_df.groupby('roi_label'))):
 
-            suffix = f'{stat}-clusters'
-            clusters_filename = f'task-{TASK}_space-{SPACE}_desc-{contrast}_{suffix}.nii.gz'
-            clusters_file = UNIVARIATE_DIR / clusters_filename
-            clusters_img = load_img(clusters_file)
-
-            plt.subplots_adjust(wspace=0.05)
-
-            plot = plot_glass_brain(clusters_img, display_mode='lyrz',
-                                    colorbar=False, figure=fig,
-                                    axes=axs[row_ix, col_ix], black_bg=False,
-                                    cmap='blue_orange', vmin=-10.0, vmax=10.0,
-                                    plot_abs=False)
-
-            contrast_label = contrast.\
-                replace('-', ' ').\
-                replace('minus', 'vs.').\
-                replace('audios', 'Spoken').\
-                replace('images', 'Written').\
+            roi_img = roi_imgs[roi_label]
+            roi_ax_left = 0.4 if ix % 2 == 0 else 0.9
+            roi_ax_bottom = 0.85 - (ix // 2) / n_rows
+            roi_ax_width = 0.09
+            roi_ax_height = 0.09
+            roi_ax = fig.add_axes([roi_ax_left, roi_ax_bottom,
+                                   roi_ax_width, roi_ax_height])
+            plot_glass_brain(roi_img, display_mode='z', colorbar=False,
+                             black_bg=False, axes=roi_ax)
+            roi_title = roi_label.\
+                replace('audios-', 'spoken\n').\
+                replace('images-', 'written\n').\
                 replace('noise', 'low-level').\
-                replace('pseudo', 'pseudowords')
-            letter = ascii_uppercase[letter_ix]
-            letter_ix += 1
-            title = f'$\\bf{{{letter}}}$  {contrast_label}, {stat_label}'
-            plot.title(title, color='black', bgcolor='white', x=0.002, y=1.1)
+                replace('pseudo', 'pseudow.').\
+                replace('words', 'word')
+            if '-left' in roi_title:
+                roi_title = roi_title.replace('-left', '')
+                roi_title = f'Left {roi_title}'
+            elif '-right' in roi_title:
+                roi_title = roi_title.replace('-right', '')
+                roi_title = f'Right {roi_title}'
+            roi_title = f'{roi_title} ROI'
+            roi_ax.set_title(roi_title)
 
-    desc = auditory_contrast.replace('audios-', '')
-    fig_filename = f'task-{TASK}_space-{SPACE}_desc-{desc}_clusters.png'
-    fig_file = fig_dir / fig_filename
-    fig.savefig(fig_file, dpi=600, bbox_inches='tight')
+            plt.sca(ax)
+            sns.lineplot(roi_df, x='time', y='r', hue='subject', palette='hls',
+                         marker='o', markeredgewidth=0, legend=False)
 
-    # Line plot for the largest significant cluster (from linear or quadratic change)
-    letter_ix = 6
-    fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(19.163, 4.0))
-    for col_ix, contrast in enumerate([auditory_contrast, visual_contrast]):
-
-        plt.sca(axs[col_ix])
-
-        # Start with empty mask
-        cluster_mask = math_img('img - img', img=clusters_img)
-        max_cluster_size = 0
-        max_cluster_stat = None
-
-        for stat in stats:
-
-            if stat == 'z0':
-                continue
-
-            suffix = f'{stat}-clusters'
-            clusters_filename = f'task-{TASK}_space-{SPACE}_desc-{contrast}_{suffix}.nii.gz'
-            clusters_file = UNIVARIATE_DIR / clusters_filename
-            clusters_img = load_img(clusters_file)
-            clusters_data = clusters_img.get_fdata()
-            clusters_data = clusters_data[clusters_data != 0.0]
-
-            if not clusters_data.any():
-                continue
-
-            cluster_ixs, cluster_counts = np.unique(clusters_data,
-                                                    return_counts=True)
-            if cluster_counts.max() > max_cluster_size:
-                max_cluster_ix = cluster_ixs[np.argmax(cluster_counts)]
-                cluster_mask = math_img(f'img == {max_cluster_ix}',
-                                        img=clusters_img)
-                max_cluster_stat = stat
-
-        if max_cluster_stat is None:
-
-            plt.axis('off')
-
-        else:
-
-            fig_filename = f'task-{TASK}_space-{SPACE}_desc-{contrast}_cluster-mask.png'
-            fig_file = fig_dir / fig_filename
-            plot_glass_brain(cluster_mask, output_file=fig_file,
-                             display_mode='lyrz', colorbar=False, black_bg=False,
-                             title=f'{contrast} cluster mask (from {max_cluster_stat})')
-
-            voxel_ixs = np.where(cluster_mask.get_fdata())
-
-            beta_dfs = []
-            for row in meta_df.itertuples():
-
-                subject = row.subject
-                session = row.session
-                func_dir = UNIVARIATE_DIR / f'sub-{subject}/ses-{session}/func'
-                beta_filename = f'sub-{subject}_ses-{session}_task-{TASK}_space-{SPACE}_desc-{contrast}_effect_size.nii.gz'
-                beta_file = func_dir / beta_filename
-                beta_img = load_img(beta_file)
-                betas = beta_img.get_fdata()[voxel_ixs]
-                beta_df = pd.DataFrame({'subject': subject,
-                                        'session': session,
-                                        'time': row.time,
-                                        'time2': row.time2,
-                                        'beta': betas.mean()},
-                                       index=[0])
-                beta_dfs.append(beta_df)
-
-            beta_df = pd.concat(beta_dfs, ignore_index=True)
-
-            sns.lineplot(beta_df, x='time', y='beta', hue='subject',
-                         palette='hls', marker='o', markeredgewidth=0,
-                         legend=False)
-
-            plt.subplots_adjust(wspace=0.15)
-            plt.xlim(-1.0, 17.0)
-            plt.xlabel('Time (months)')
-            plt.ylabel('BOLD amplitude (a.u.)')
-
-            bs = []
-            for stat in stats:
-                b_stat = stat.replace('z', 'b')
-                b_filename = f'task-{TASK}_space-{SPACE}_desc-{contrast}_{b_stat}.nii.gz'
-                b_file = UNIVARIATE_DIR / b_filename
-                b_img = load_img(b_file)
-                b_vals = b_img.get_fdata()[voxel_ixs]
-                bs.append(b_vals.mean())
-            add_quadratic_line(*bs)
+            this_stat_df = stat_df.query(f'pair_label == "{pair_label}" & ' +
+                                         f'roi_label == "{roi_label}"')
+            intercept = this_stat_df.loc[this_stat_df['effect']
+                                         == 'intercept', 'beta'].values[0]
+            linear = this_stat_df.loc[this_stat_df['effect']
+                                      == 'linear', 'beta'].values[0]
+            quadratic = this_stat_df.loc[this_stat_df['effect']
+                                         == 'quadratic', 'beta'].values[0]
+            add_quadratic_line(intercept, linear, quadratic)
 
             letter = ascii_uppercase[letter_ix]
             letter_ix += 1
             title = f'$\\bf{{{letter}}}$'
-            plt.title(title, color='black', x=-0.08, y=0.93)
+            plt.title(title, color='black', x=-0.095, y=0.93)
 
-        fig_filename = f'task-{TASK}_space-{SPACE}_desc-{desc}_lineplot.png'
+            plt.xlim(-1.0, 17.0)
+            plt.ylim(-1.1, 1.1)
+            plt.xlabel('Time (months)')
+            plt.ylabel('Correlation coefficient')
+
+            if ix == 0:
+                pair_title_label = pair_label.\
+                    replace('noise', 'low-level').\
+                    replace('pseudo', 'pseudowords').\
+                    replace('-minus-', ' vs. ')
+                pair_title = f'Audio-visual pattern similarity for {pair_title_label}'
+                plt.annotate(pair_title, xy=(0.01, 0.93),
+                             xycoords='axes fraction', size=14.0)
+
+        # plt.subplots_adjust(wspace=10.0)
+        plt.tight_layout()
+
+        fig_filename = f'task-{TASK}_space-{SPACE}_desc-{pair_label}_similarity.png'
         fig_file = fig_dir / fig_filename
         fig.savefig(fig_file, dpi=600, bbox_inches='tight')
+
+
+if __name__ == '__main__':
+    main()
