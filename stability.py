Fix stability calculation

stability.py
@@ -4,10 +4,10 @@ from warnings import warn
 import numpy as np
 import pandas as pd
 from bids import BIDSLayout
-from similarity import ATLAS_FILE, get_roi_maskers
+from similarity import ATLAS_FILE, GLASSER_ROIS, get_roi_maskers
 from univariate import (BIDS_DIR, CONTRASTS, DERIVATIVES_DIR, DF_QUERY,
-                        FD_THRESHOLD, FMRIPREP_DIR, N_JOBS, PYBIDS_DIR, SPACE,
-                        TASK, compute_beta_img, load_df, run_glms)
+                        FD_THRESHOLD, FMRIPREP_DIR, PYBIDS_DIR, SPACE, TASK,
+                        compute_beta_img, load_df, run_glms)
 
 # Input parameters: Input/output directories
 STABILITY_DIR = DERIVATIVES_DIR / 'stability'
@@ -17,6 +17,7 @@ BLOCKWISE = True
 HRF_MODEL = 'glover'
 SMOOTHING_FWHM = None
 SAVE_RESIDUALS = False
+N_JOBS = 4
 
 
 def main():
@@ -44,26 +45,35 @@ def main():
 
     # Get anatomical and functional regions of interest
     roi_maskers = get_roi_maskers(ATLAS_FILE, ref_img=mask_imgs[0])
+    anat_roi_labels = list(GLASSER_ROIS.keys())
+    func_roi_labels = [roi_label for roi_label in roi_maskers.keys()
+                       if roi_label not in anat_roi_labels]
 
     # Run pattern stability analysis
-    corr_df = compute_stability(subjects, sessions, glms, roi_maskers)
+    corr_df = compute_stability(subjects, sessions, glms,
+                                roi_maskers, anat_roi_labels)
 
     # Save results
+    STABILITY_DIR.mkdir(exist_ok=True, parents=True)
     df = pd.merge(df, corr_df, on=['subject', 'session'])
     df_filename = f'task-{TASK}_space-{SPACE}_desc-stability.tsv'
     df_file = STABILITY_DIR / df_filename
     df.to_csv(df_file, sep='\t', index=False, float_format='%.4f')
 
 
-def compute_stability(subjects, sessions, glms, roi_maskers):
+def compute_stability(subjects, sessions, glms, roi_maskers, anat_roi_labels):
     """For each subject, session, condition, and ROI, computes the within-
     condition pattern stability (i.e., the average correlation between pairs
     of trials from the same condition)."""
 
+    baseline_contrasts = {contrast_label: (conditions_plus, conditions_minus)
+                          for contrast_label, (conditions_plus, conditions_minus)
+                          in CONTRASTS.items() if len(conditions_minus) == 0}
+
     corr_dfs = []
     for subject, session, glm in zip(subjects, sessions, glms):
 
-        for contrast_label, (conditions_plus, conditions_minus) in CONTRASTS.items():
+        for contrast_label, (conditions_plus, conditions_minus) in baseline_contrasts.items():
 
             if len(conditions_minus) > 0:
 
@@ -91,16 +101,20 @@ def compute_stability(subjects, sessions, glms, roi_maskers):
 
             pairs = list(combinations(beta_imgs, 2))
 
-            for roi_label, roi_masker in roi_maskers.items():
+            this_func_roi_labels = [roi_label for roi_label in roi_maskers.keys()
+                                    if roi_label.startswith(contrast_label)]
+            this_roi_labels = anat_roi_labels + this_func_roi_labels
+            this_roi_maskers = {roi_label: roi_masker
+                                for roi_label, roi_masker in roi_maskers.items()
+                                if roi_label in this_roi_labels}
 
-                corrs = []
-                for pair in pairs:
+            for roi_label, roi_masker in this_roi_maskers.items():
 
-                    betas = [roi_masker.transform(beta_img)[0]
-                             for beta_img in pair]
-                    corr = np.corrcoef(betas)[0, 1]
-                    corrs.append(corr)
+                betas = [roi_masker.transform(beta_img)[0]
+                         for beta_img in beta_imgs]
 
+                pairs = list(combinations(betas, 2))
+                corrs = [np.corrcoef(pair)[0, 1] for pair in pairs]
                 corr = np.mean(corrs)
 
                 corr_df = pd.DataFrame({'subject': subject,
@@ -112,3 +126,7 @@ def compute_stability(subjects, sessions, glms, roi_maskers):
                 corr_dfs.append(corr_df)
 
     return pd.concat(corr_dfs, ignore_index=True)
+
+
+if __name__ == '__main__':
+    main()